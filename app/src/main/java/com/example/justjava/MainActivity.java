28d7174a036bdd14def30b5d1aee9c19d8787ac0
Arjun Nair <nair.arjun47@yahoo.com>
2020-06-25 13:09:17 -0700

    Completed lesson

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -50,7 +50,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Total = $" + (quantity * 5);
+        String priceMessage = "Total: $" + (quantity * 5);
+        priceMessage = priceMessage + "\nThank You!";
         displayMessage(priceMessage);
     }
 
